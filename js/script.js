52ecdd919b1fe027282b98a354bf54f9c9ad036a
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-06-27 14:03:46 +0200

    milestone 1 completed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
 /*
-Creato un array contenente una lista di cinque immagini tra quelle fornite, creare un carosello ispirandoci alle foto in allegato.
-(non è necessario che la grafica sia IDENTICA alla traccia, non bloccatevi su questo, date un aspetto decente e concentriamoci sulla logica.
-Cerchiamo di lavorare seguendo i seguenti step in ordine:
 MILESTONE 1
 Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider; avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
 MILESTONE 2
